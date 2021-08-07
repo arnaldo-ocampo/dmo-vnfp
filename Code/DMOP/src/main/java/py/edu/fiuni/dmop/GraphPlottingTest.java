48544edbb9d367e0cd42e46674a4c22d6f86662b
arnaldo.ocampo <arnaldo.ocampo@fiuni.edu.py>
2021-08-06 23:43:52 -0400

    Update GraphPlottingTest.java

--- Code/DMOP/src/main/java/py/edu/fiuni/dmop/GraphPlottingTest.java
+++ Code/DMOP/src/main/java/py/edu/fiuni/dmop/GraphPlottingTest.java
@@ -5,19 +5,20 @@
  */
 package py.edu.fiuni.dmop;
 
-import java.util.HashMap;
 import java.util.Map;
 import org.apache.log4j.Logger;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
+
 import py.edu.fiuni.dmop.dto.NFVdto.Link;
-import py.edu.fiuni.dmop.service.DMOPService;
 import py.edu.fiuni.dmop.service.DataService;
 import py.edu.fiuni.dmop.util.Configurations;
 
 /**
- *
- * @author Arnaldo
+ * Reads the network topology and then it plots it as Graph
+ * 
+ * @author Arnaldo Ocampo
+ * @author Nestor Tapia
  */
 public class GraphPlottingTest {
 
@@ -26,47 +27,23 @@ public class GraphPlottingTest {
     public static void main(String args[]) {
 
         try {
-            System.setProperty("org.graphstream.ui", "swing");
-
-            /*
-        Graph graph = new SingleGraph("Tutorial 1");
-
-        Node n = graph.addNode("A");
-        //n.setAttribute("ui.label", "A");
-        graph.addNode("B");
-        graph.addNode("C");
-        graph.addEdge("AB", "A", "B");
-        graph.addEdge("BC", "B", "C");
-        graph.addEdge("CA", "C", "A");
-
-       
-
-        graph.display();*/
-            Graph graph = new SingleGraph("tutorial 1");
-
-            graph.setAttribute("ui.stylesheet", "node {shape: box; size: 45px, 20px; fill-mode: plain; fill-color: lightgrey; stroke-mode: plain; stroke-color: #333;}");
-
-            //graph.setStrict(false);
-            //graph.setAutoCreate(true);
-            /*graph.addEdge("AB", "A", "B");
-        graph.addEdge("BC", "B", "C");
-        graph.addEdge("CA", "C", "A");
-        graph.addEdge("AD", "A", "D");
-        graph.addEdge("DE", "D", "E");
-        graph.addEdge("DF", "D", "F");
-        graph.addEdge("EF", "E", "F");*/
-            // graph.setAttribute("ui.stylesheet", "node#A {shape: box; size: 45px, 20px; fill-mode: plain; fill-color: lightgrey; stroke-mode: plain; stroke-color: grey;}");
-///        node#A {shape: box; size: 15px, 20px; fill-mode: plain; fill-color: red; stroke-mode: plain; stroke-color: blue;}
+            
             Configurations.loadProperties();
             DataService.loadData();
 
             Map<String, py.edu.fiuni.dmop.dto.NFVdto.Node> nodesMap = DataService.nodesMap;
             Map<String, Link> linksMap = DataService.linksMap;
-
+            
+            System.setProperty("org.graphstream.ui", "swing");
+            Graph graph = new SingleGraph("Network Topology");
+            graph.setAttribute("ui.stylesheet", "node {shape: box; size: 45px, 20px; fill-mode: plain; fill-color: lightgrey; stroke-mode: plain; stroke-color: #333;}");
+            
+            // Adds every node to the graph
             nodesMap.values().forEach(n -> {
                 graph.addNode(n.getId());
             });
 
+            // Adds every edge (link) between neighbour nodes to the graph
             linksMap.values().forEach(l -> {
                 String linkId = l.getId();
                 String nodesId = linkId.substring(0, linkId.indexOf("/"));
@@ -75,12 +52,34 @@ public class GraphPlottingTest {
                 graph.addEdge(nodesId, ids[0], ids[1]);
             });
 
+            // Configure the graph to show a label next to every node
             for (Node node : graph) {
                 node.setAttribute("ui.label", node.getId());
             }
 
             graph.display();
 
+            /*
+            Graph graph = new SingleGraph("Network Topology");
+            graph.setAttribute("ui.stylesheet", "node {shape: box; size: 45px, 20px; fill-mode: plain; fill-color: lightgrey; stroke-mode: plain; stroke-color: #333;}");
+
+            graph.setStrict(false);
+            graph.setAutoCreate(true);
+            graph.addEdge("AB", "A", "B");
+            graph.addEdge("BC", "B", "C");
+            graph.addEdge("CA", "C", "A");
+            graph.addEdge("AD", "A", "D");
+            graph.addEdge("DE", "D", "E");
+            graph.addEdge("DF", "D", "F");
+            graph.addEdge("EF", "E", "F");
+            graph.setAttribute("ui.stylesheet", "node#A {shape: box; size: 45px, 20px; fill-mode: plain; fill-color: lightgrey; stroke-mode: plain; stroke-color: grey;}");
+
+            for (Node node : graph) {
+                node.setAttribute("ui.label", node.getId());
+            }
+
+            graph.display();
+            */
         } catch (Exception ex) {
             logger.error(ex);
         }
